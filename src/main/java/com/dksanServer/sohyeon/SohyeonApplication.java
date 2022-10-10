2159eb2e89455be896566efd7b2bc82a851f6993
thguss <thgus345@naver.com>
2022-10-10 19:25:33 +0900

    [INIT] 초기 프로젝트 세팅

---
+++ src/main/java/com/dksanServer/sohyeon/SohyeonApplication.java
@@ -0,0 +1,13 @@
+package com.dksanServer.sohyeon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SohyeonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SohyeonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dksanServer/sohyeon/SohyeonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dksanServer.sohyeon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SohyeonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
