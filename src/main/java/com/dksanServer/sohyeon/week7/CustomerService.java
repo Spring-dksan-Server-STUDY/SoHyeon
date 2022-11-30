5c882044198d209bc8baf6b59bf5491e45bd98be
김소현 <thgus345@naver.com>
2022-12-01 01:57:10 +0900

    [week7] 스프링 스터디 7주차 자료

---
+++ src/main/java/com/dksanServer/sohyeon/week7/CustomerService.java
@@ -0,0 +1,18 @@
+package com.dksanServer.sohyeon.week7;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+public interface CustomerService {
+
+    Customer createCustomer(String name, int age, String email);
+
+    List<Customer> getAllCustomers();
+
+    Optional<Customer> getCustomerById(UUID customerId);
+
+    Customer updateCustomer(String name, int age, String email);
+
+    void deleteAllCustomers();
+}
